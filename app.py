746dd34e2fda0a3fe650378208fba022d0710640
George Lawrence <georgeblawrence@hotmail.co.uk>
2023-01-22 10:02:27 +0000

    Add test button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -6,5 +6,10 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def hello():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    text_field = request.form['text_field']
+    return f"you entered {text_field}"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,2 @@
+const inputContainer = document.getElementById('input-container');
+inputContainer.innerHTML = `You entered: ${text_field}`;
